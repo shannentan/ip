cf54fa9920d0a8dc74c2825ae58530ac345d07d4
SHANNEN\shan <shannen.tan@u.nus.edu>
2023-01-26 18:02:16 +0800

    Add Level-2 Add, Lisr

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Duke!\n" + "How may I help you today?");
+        Scanner response = new Scanner(System.in);
+        String line = response.nextLine();
+        int counter = 0;
+        String[] items = new String[100];
+
+        while (!(line.equals("bye"))) {
+            if (line.equals("list")) {
+                for (int a = 0; a < counter; a++) {
+                    System.out.println((a + 1) + ". " + items[a]);
+                }
+                line = response.nextLine();
+            } else {
+                System.out.println("added: " + line);
+                items[counter] = line;
+                line = response.nextLine();
+                counter++;
+            }
+        }
+        System.out.println("Bye! Have a nice day:D");
+    }
+}
+
+
+/* public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,3 +40,4 @@ public class Duke {
                 " What can I do for you?\n" +  "Bye. Hope to see you again soon!");
     }
 }
+*/
