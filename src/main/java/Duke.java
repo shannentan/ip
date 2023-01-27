2a4c97086a46b05335104bcf33152b7f58ccfd02
SHANNEN\shan <shannen.tan@u.nus.edu>
2023-01-27 15:55:56 +0800

    Add Level-3 Mark as Done, Add A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,41 +3,43 @@ import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
-        System.out.println("Hello, I'm Duke!\n" + "How may I help you today?");
+        System.out.println("Hello, I'm Duke!\n" + "What would you like to do today?");
         Scanner response = new Scanner(System.in);
         String line = response.nextLine();
         int counter = 0;
-        String[] items = new String[100];
+        String[] items = new String[101]; // 1-based
+        String[] check = new String[101]; // 1-based
 
         while (!(line.equals("bye"))) {
             if (line.equals("list")) {
-                for (int a = 0; a < counter; a++) {
-                    System.out.println((a + 1) + ". " + items[a]);
+                System.out.println("...\n" + "Here are the tasks in your list:");
+                for (int a = 1; a <= counter; a++) {
+                    String checkOrNot = "x";
+                    if ((check[a] == null) || (check[a] == " ")) {
+                        checkOrNot = (" ");
+                    }
+                    System.out.println((a) + ". " + "[" + checkOrNot + "] " + items[a]);
                 }
-                line = response.nextLine();
+            } else if (line.startsWith("mark")) {
+                String strIndex = line.substring(5);
+                int index = Integer.parseInt(strIndex);
+                check[index] = "x";
+                System.out.println("...\n" + "Nice! I've marked this task as done:");
+                System.out.println(index);
+                System.out.println("  [" + check[index] + "] " + items[index]);
+            } else if (line.startsWith("unmark")) {
+                String strIndex = line.substring(7);
+                int index = Integer.parseInt(strIndex);
+                check[index] = " ";
+                System.out.println("...\n" + "OK, I've marked this task as not done yet:");
+                System.out.println("  [" + check[index] + "] " + items[index]);
             } else {
-                System.out.println("added: " + line);
-                items[counter] = line;
-                line = response.nextLine();
+                System.out.println("...\n" + "added: " + line);
                 counter++;
+                items[counter] = line;
             }
+            line = response.nextLine();
         }
         System.out.println("Bye! Have a nice day:D");
     }
 }
-
-
-/* public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        System.out.println("Hello! I'm Duke\n" +
-                " What can I do for you?\n" +  "Bye. Hope to see you again soon!");
-    }
-}
-*/
