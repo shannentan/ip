03150210fa94409c0c526a42cebc5c6af7368daf
SHANNEN\shan <shannen.tan@u.nus.edu>
2023-02-10 18:55:05 +0800

    Add Level-5 Handle Errors, Add A-Exceptions
    (cherry picked from commit 3e169c7892e53aa66ad368758f6a91c5afd222f3)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,49 +1,76 @@
+import exceptions.DukeException;
+
 import java.util.Scanner;
 
 public class Duke {
+//    1) check for sufficient arguments
+//    2) check for /by etc
 
     public static void main(String[] args) {
         System.out.println("Hello, I'm Duke!\n" + "What would you like to do today?");
         Scanner response = new Scanner(System.in);
-        String line = response.nextLine();
         int counter = 1;
         Task[] items = new Task[101]; // 1-based
+        String line = "";
 
         while (!(line.equals("bye"))) {
+            line = response.nextLine();
+            String[] lines = line.trim().split(" ", 2);
+            String command = lines[0];
             if (line.equals("list")) {
                 System.out.println("...\n" + "Here are the tasks in your list:");
                 for (int i = 1; i < counter; i++) {
                     System.out.println(i + "." + items[i]);
                 }
-            } else if (line.startsWith("mark")) {
-                String strIndex = line.substring(5);
-                int index = Integer.parseInt(strIndex);
-                items[index].isDone = true;
-                System.out.println("...\n" + "Nice! I've marked this task as done:\n" + items[index]);
-            } else if (line.startsWith("unmark")) {
-                String strIndex = line.substring(7);
-                int index = Integer.parseInt(strIndex);
-                items[index].isDone = false;
-                System.out.println("...\n" + "OK, I've marked this task as not done yet:\n" + items[index]);
-            } else if (line.startsWith("todo")) {
+                continue;
+            } else if (line.equals("bye")) {
+                break;
+            }
+            String arguments = lines[1];
+            if (command.equals("mark")) {
+                try {
+                    int index = Integer.parseInt(arguments);
+                    items[index].isDone = true;
+                    System.out.println("...\n" + "Nice! I've marked this task as done:\n" + items[index]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Please input a valid index!");
+                }
+            } else if (command.equals("unmark")) {
+                try {
+                    int index = Integer.parseInt(arguments);
+                    items[index].isDone = false;
+                    System.out.println("...\n" + "OK, I've marked this task as not done yet:\n" + items[index]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Please input a valid index!");
+                }
+            } else if (command.equals("todo")) {
                 Todo newTodo = new Todo(line.substring(5));
                 items[counter] = newTodo;
                 counter++;
-            } else if (line.startsWith("event")) {
+            } else if (command.equals("event")) {
                 String eventSubstring = line.trim().substring(6);
                 String[] eventInfo = eventSubstring.split("/from ");
                 String[] eventDuration = eventInfo[1].split("/to ");
                 Event newEvent = new Event(eventInfo[0], eventDuration[0], eventDuration[1]);
                 items[counter] = newEvent;
                 counter++;
-            } else if (line.startsWith("deadline")) {
-                String deadlineWithoutCommand = line.trim().substring(9);
-                String[] deadlineInfo = deadlineWithoutCommand.split("/by ");
-                Deadline newDeadline = new Deadline(deadlineInfo[0], deadlineInfo[1]);
-                items[counter] = newDeadline;
-                counter++;
+            } else if (command.equals("deadline")) {
+                try {
+                    String[] deadlineInfo = arguments.split("/by ");
+                    if (deadlineInfo.length < 2) {
+                        throw new DukeException("There's missing information. Did you miss out the description or the deadline?");
+                    }
+                    Deadline newDeadline = new Deadline(deadlineInfo[0], deadlineInfo[1]);
+                    items[counter] = newDeadline;
+                    counter++;
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("What would you like to do, and when's the deadline?");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Remember to add /by before the deadline!");
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
             }
-            line = response.nextLine();
         }
         System.out.println("Bye! Have a nice day:D");
     }
