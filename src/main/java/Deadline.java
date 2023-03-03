517e9327e4182c5d37ba98102c93c132ba884412
SHANNEN\shan <shannen.tan@u.nus.edu>
2023-03-03 13:11:46 +0800

    Remove extra code

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,10 @@
 public class Deadline extends Task {
 
     protected String by;
-//    protected String opening = "...\n" + "Got it. I've added this deadline:\n" + " [D][ ] ";
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
-//        System.out.println(opening + description + "(by: " + by + ")");
     }
 
     @Override
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,11 @@
 public class Event extends Task {
     protected String start;
     protected String end;
-//    protected String opening = "...\n" + "Got it. I've added this event:\n" + " [E][ ] ";
 
     public Event(String description, String start, String end) {
         super(description);
         this.start = start;
         this.end = end;
-//        System.out.println(opening + description + "(from: " + start + " to: " + end + ")");
     }
 
     @Override
